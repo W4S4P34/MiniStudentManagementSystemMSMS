9caefc11ed31812deed553b1bba26ca4c5864fe6
Thiện Bùi <thien.bui.84436@gmail.com>
2019-03-12 14:15:37 +0700

    tách ra thành _FILES_.h

---
+++ _FILES_.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Logins
+constexpr auto LOGIN_LIST_ADMIN = "D:/data/Login/Admin.txt";
+constexpr auto LOGIN_LIST_LECTURER = "D:/data/Login/Lecturer.txt";
+constexpr auto LOGIN_LIST_STUDENT = "D:/data/Login/Student.txt";
