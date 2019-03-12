11a22e7e447bdfa9884b59105053e7c9fdad1324
Thiện Bùi <thien.bui.84436@gmail.com>
2019-03-12 10:56:22 +0700

    xây lại codebase cho login

--- Login.cpp
+++ Login.cpp
@@ -1,82 +1,130 @@
 #include "Login.h"
 
-// Student: ID-BirthDM
-// Lecturer: the usual (nhminh)
-// Admin: admin1337
-char LoginMenu() {
-	char r;
+void LoginMenu(string & ID, char & AccessClass) {
+	cout << "(Type \"quit\" in ID to quit.)\n\n";
 	do {
-		string username, password;
-		cout << "(Type \"quit\" in username to quit.)\n";
-		cout << "Username: ";
-		cin >> username;
-		if (username == "quit") exit(EXIT_SUCCESS);
+		
+		cout << "ID: ";
+		getline(cin, ID);
+		if (ID == "quit") exit(EXIT_SUCCESS);
+		string Password;
 		cout << "Password: ";
-		cin >> password;
-		r = Authenticate(username, password);
-	} while (r == -1 && cout << "Invalid login details.\n\n");
-	return r;
+		// getline(cin, Password);
+		Password = GetPassword();
+		AccessClass = Authenticate(ID, Password);
+	} while (
+		AccessClass == AC_INVALID
+		&& cout << "\nInvalid login details.\n\n"
+	);
 }
 
-// 0=admin, 1=student, 2=lecturer, -1=invalid
-char Authenticate(string username, string password) {
+string GetPassword() {
+	const char BACKSPACE = 8; // BS
+	const char RETURN = 13; // CR; 10=LF
+
+	string Password;
+	unsigned char c;
+	while ((c = _getch()) != RETURN) {
+		if (c == BACKSPACE) {
+			if (Password.length() != 0) {
+				cout << "\b \b";
+				Password.resize(Password.size() - 1);
+				// Password.pop_back() : C++11
+			}
+		}
+		else if (c == 0 || c == 224) {
+			_getch();
+			continue;
+		}
+		else if (c >= 32 && c <= 126) {
+			cout << " \b*";
+			Password.push_back(c);
+		}
+	}
+	return Password;
+}
+
+char Authenticate(const string & ID, const string & Password) {
 	ifstream LoginList;
+	char AccessClass;
 
-	// ADMIN
-	if (username.substr(0, 5) == "admin") {
-		LoginList.open(LOGIN_ADMIN);
-		if (!LoginList.is_open()) {
-			cout << "Unable to open file for login. Forcefully exiting...\n";
-			exit(EXIT_FAILURE);
-		}
-		while (!LoginList.eof()) {
-			string usernameToCompare, passwordToCompare;
-			getline(LoginList, usernameToCompare, ',');
-			getline(LoginList, passwordToCompare);
-			if (username == usernameToCompare && password == passwordToCompare)
-				return 0;
-			else
-				return -1;
-		}
+	if (ID.substr(0, 5) == "admin") {
+		LoginList.open(LOGIN_LIST_ADMIN);
+		AccessClass = AC_ADMIN;
 	}
-
-	// STUDENT
-	else if (username[0] >= '1' && username[0] <= '9') {
-		LoginList.open(LOGIN_STUDENT);
-		if (!LoginList.is_open()) {
-			cout << "Unable to open file for login. Forcefully exiting...\n";
-			exit(EXIT_FAILURE);
-		}
-		while (!LoginList.eof()) {
-			string usernameToCompare, passwordToCompare;
-			getline(LoginList, usernameToCompare, ',');
-			getline(LoginList, passwordToCompare);
-			if (username == usernameToCompare && password == passwordToCompare)
-				return 1;
-			else
-				return -1;
-		}
+	else if ((ID[0] >= 'A' && ID[0] <= 'Z') || (ID[0] >= 'a' && ID[0] <= 'z')) {
+		LoginList.open(LOGIN_LIST_LECTURER);
+		AccessClass = AC_LECTURER;
 	}
-
-	// LECTURER
-	else if ((username[0] >= 'a' &&  username[0] <= 'z') || (username[0] >= 'A' && username[0] <= 'Z')) {
-		LoginList.open(LOGIN_LECTURER);
-		if (!LoginList.is_open()) {
-			cout << "Unable to open file for login. Forcefully exiting...\n";
-			exit(EXIT_FAILURE);
-		}
-		while (!LoginList.eof()) {
-			string usernameToCompare, passwordToCompare;
-			getline(LoginList, usernameToCompare, ',');
-			getline(LoginList, passwordToCompare);
-			if (username == usernameToCompare && password == passwordToCompare)
-				return 2;
-			else
-				return -1;
-		}
+	else if (ID[0] >= '1' && ID[0] <= '9') {
+		LoginList.open(LOGIN_LIST_STUDENT);
+		AccessClass = AC_STUDENT;
 	}
-
 	else {
 		return -1;
 	}
+
+	if (!LoginList.is_open()) {
+		cerr << "ERROR: Unable to open login list. Forcefully quitting...\n";
+		exit(EXIT_FAILURE);
+	}
+
+	string ID_compare, Password_compare;
+	while (!LoginList.eof()) {
+		getline(LoginList, ID_compare, ',');
+		getline(LoginList, Password_compare, '\n');
+		if (ID_compare == ID && sha1(Password) == Password_compare) {
+				LoginList.close();
+				return AccessClass;
+		}
+	}
+	return -1;
+}
+
+void ChangePassword(const string & ID, const char & AccessClass) {
+	fstream LoginList;
+	switch (AccessClass) {
+		case AC_ADMIN:
+			LoginList.open(LOGIN_LIST_ADMIN);
+			break;
+		case AC_LECTURER:
+			LoginList.open(LOGIN_LIST_LECTURER);
+			break;
+		case AC_STUDENT:
+			LoginList.open(LOGIN_LIST_STUDENT);
+			break;
+	}
+
+	if (!LoginList.is_open()) {
+		cerr << "ERROR: Unable to open login list. Forcefully quitting...\n";
+		exit(EXIT_FAILURE);
+	}
+
+	string Password_new;
+	string Password_new_re;
+	cout << "Enter new password: ";
+	getline(cin, Password_new);
+	cout << "Re-enter new password: ";
+	getline(cin, Password_new_re);
+
+	if (Password_new != Password_new_re) {
+		cerr << "Passwords do not match. Operation aborted.\n";
+	}
+	else {
+		string ID_compare, skip;
+		streamoff SeekPosition;
+		while (!LoginList.eof()) {
+			SeekPosition = LoginList.tellg();
+			getline(LoginList, ID_compare, ',');
+			getline(LoginList, skip);
+			if (ID_compare == ID) {
+				LoginList.seekg(SeekPosition);
+				LoginList << ID << ',' << sha1(Password_new);
+				cout << "Password changed.";
+				break;
+			}
+		}
+	}
+
+	LoginList.close();
 }
--- Login.h
+++ Login.h
@@ -1,13 +1,26 @@
 #pragma once
-
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <conio.h>
+#include "sha1/sha1.h"
 using namespace std;
 
-#define LOGIN_STUDENT "D:/Login.Student.csv"
-#define LOGIN_LECTURER "D:/Login.Lecturer.csv"
-#define LOGIN_ADMIN "D:/Login.Admin.csv"
+// SHA-1: 80 bits
+// Empty password: da39a3ee5e6b4b0d3255bfef95601890afd80709
 
-char LoginMenu();
-char Authenticate(string username, string password);
+// File list
+constexpr auto LOGIN_LIST_ADMIN = "D:/data/Login/Admin.txt";
+constexpr auto LOGIN_LIST_LECTURER = "D:/data/Login/Lecturer.csv";
+constexpr auto LOGIN_LIST_STUDENT = "D:/data/Login/Student.csv";
+
+// Access class
+constexpr auto AC_ADMIN = 0;
+constexpr auto AC_LECTURER = 1;
+constexpr auto AC_STUDENT = 2;
+constexpr auto AC_INVALID = -1;
+
+void LoginMenu(string & ID, char & AccessClass); // also returns access class
+string GetPassword(); // masked password field
+char Authenticate(const string & ID, const string & Password);
+void ChangePassword(const string & ID, const char & AccessClass);
