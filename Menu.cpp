c5672572988790f49edec6d610da014147214823
Thiện Bùi <thien.bui.84436@gmail.com>
2019-03-12 10:56:29 +0700

    xây lại codebase cho menu

--- Menu.cpp
+++ Menu.cpp
@@ -1,171 +1,156 @@
-﻿#include "Menu.h"
+#include "Menu.h"
 
-/*----------------------------------------------------------------*/
-
-char Menu_Student() {
-	cout << "STUDENT MENU (Note: choices are case-insensitive.)\n\n"
+void ShowMenu(const char & AccessClass) {
+	cout << "COMMAND LIST:\n"
 		<< "[-] Log out\n"
+		<< "[I] Show user info\n"
+		<< "[?] Show this command list\n"
 		<< "[*] Change password\n"
-		<< "\n"
-		<< "[C] Check-in\n"
-		<< "[H] View check-in result\n"
-		<< "[S] View schedules\n"
-		<< "[R] View scoreboard result\n"
-		<< endl;
+		<< "[CLS] Clear the screen\n"
+		<< "\n";
 
-	char r;
-	do {
-		cout << "> Choose: ";
-		string c;
-		cin >> c;
-		transform(c.begin(), c.end(), c.begin(), ::tolower);
+	switch (AccessClass) {
+		case AC_STUDENT:
+			cout << "------------------------------\n\n"
+				<< "[C] Check-in\n"
+				<< "[H] View check-in result\n"
+				<< "[S] View schedules\n"
+				<< "[R] View scoreboard result\n";
+			break;
 
-		if (c == "-") { r = -2; cout << "Logging out..."; }
-		else if (c == "*") { r = 0; cout << c; }
-		else if (c == "c") { r = 0; cout << c; }
-		else if (c == "h") { r = 0; cout << c; }
-		else if (c == "s") { r = 0; cout << c; }
-		else if (c == "r") { r = 0; cout << c; }
+		case AC_LECTURER:
+			cout << "------------------------------\n\n"
+				<< "[C1] View list of courses in the current semester\n"
+				<< "[C2] View list of students of a course\n"
+				<< "[A1] View attendance list of a course\n"
+				<< "[A2] Edit an attendance\n"
+				<< "[S1] Import scoreboard of a course (midterm, final, lab, bonus) from a csv file\n"
+				<< "[S2] Edit grade of a student\n"
+				<< "[S3] View a scoreboard\n";
+			break;
 
-		else { r = -1; cout << "Invalid choice.\n"; }
-	} while (r == -1);
-	return r;
+		case AC_ADMIN:
+			cout << "------------------------------\n\n"
+				<< "STUDENTS AND CLASSES\n"
+				<< "[T1] Import students of a class (from a CSV file)\n"
+				<< "[T2] Add a student to a class\n"
+				<< "[T3] Change a student from class A to class B\n"
+				<< "[T4] Edit a student\n"
+				<< "[T5] Remove a student\n"
+				<< "[T6] View list of classes\n"
+				<< "[T7] View list of students in a class\n"
+				<< "\n"
+				<< "ACADEMIC YEARS AND SEMESTERS\n"
+				<< "[Y1] Create an academic year\n"
+				<< "[Y2] Update an academic year\n"
+				<< "[Y3] Delete an academic year\n"
+				<< "[Y4] View an academic year\n"
+				<< "[Y5] Create a semester\n"
+				<< "[Y6] Update a semester\n"
+				<< "[Y7] Delete a semester\n"
+				<< "[Y8] View a semester\n"
+				<< "\n"
+				<< "COURSES\n"
+				<< "[C1] Create a new course\n"
+				<< "[C2] Edit a course\n"
+				<< "[C3] Delete a course\n"
+				<< "[C4] Add a student to a course\n"
+				<< "[C5] Remove a student from a course\n"
+				<< "[C6] View list of courses in the current semester\n"
+				<< "[C7] View list of students of a course\n"
+				<< "\n"
+				<< "LECTURERS\n"
+				<< "[L1] Create a lecturer\n"
+				<< "[L2] Update a lecturer's info\n"
+				<< "[L3] Delete a lecturer\n"
+				<< "[L4] View all lecturers\n"
+				<< "\n"
+				<< "SCOREBOARDS\n"
+				<< "[S1] Search scoreboard of a course\n"
+				<< "[S2] View scoreboard of a course\n"
+				<< "[S3] Export a scoreboard (to a Csv file)\n"
+				<< "\n"
+				<< "ATTENDANCE LISTS\n"
+				<< "[A1] Search attendance list of a course\n"
+				<< "[A2] View attendance list of a course\n"
+				<< "[A3] Export attendance list (to a CSV file)\n";
+			break;
+	}
+	
+	cout << "\nNote: invalid command will return no error.";
 }
 
-/*----------------------------------------------------------------*/
-
-char Menu_Lecturer() {
-	cout << "LECTURER MENU (Note: choices are case-insensitive.)\n\n"
-		<< "[-] Log out\n"
-		<< "[*] Change password\n"
-		<< "\n"
-		<< "[C] View list of courses in the current semester\n"
-		<< "[S] View list of students of a course\n"
-		<< "[A] View attendance list of a course\n"
-		<< "[T] Edit an attendance\n"
-		<< "[I] Import scoreboard of a course (midterm, final, lab, bonus) from a csv file\n"
-		<< "[R] Edit grade of a student\n"
-		<< "[O] View a scoreboard\n"
-		<< endl;
-
-	char r;
+void Interpret(const string & ID, const char & AccessClass) {
 	do {
-		cout << "> Choose: ";
 		string c;
-		cin >> c;
-		transform(c.begin(), c.end(), c.begin(), ::tolower);
+		cout << "(" << ID << ")>";
+		getline(cin, c);
 
-		if (c == "-") { r = -2; cout << "Logging out..."; }
-		else if (c == "*") { r = 0; cout << c; }
-		else if (c == "c") { r = 0; cout << c; }
-		else if (c == "s") { r = 0; cout << c; }
-		else if (c == "a") { r = 0; cout << c; }
-		else if (c == "t") { r = 0; cout << c; }
-		else if (c == "i") { r = 0; cout << c; }
-		else if (c == "r") { r = 0; cout << c; }
-		else if (c == "o") { r = 0; cout << c; }
+		// case-insensitivity
+		for (size_t i = 0; i < c.length(); i++)
+			c[i] = ::tolower(c[i]);
 
-		else { r = -1; cout << "Invalid choice.\n"; }
-	} while (r == -1);
-	return r;
-}
+		if (c == "-") break;
+		else if (c == "i") cout << "Show user information";
+		else if (c == "?") ShowMenu(AccessClass);
+		else if (c == "cls") system("CLS");
+		else if (c == "*") ChangePassword(ID, AccessClass);
 
-/*----------------------------------------------------------------*/
+		else switch (AccessClass) {
+			case AC_STUDENT:
+				if (c == "c") { cout << c; }
+				else if (c == "h") { cout << c; }
+				else if (c == "s") { cout << c; }
+				else if (c == "r") { cout << c; }
+				break;
 
+			case AC_LECTURER:
+				if (c == "c1") { cout << c; }
+				else if (c == "c2") { cout << c; }
+				else if (c == "a1") { cout << c; }
+				else if (c == "a2") { cout << c; }
+				else if (c == "s1") { cout << c; }
+				else if (c == "s2") { cout << c; }
+				else if (c == "s3") { cout << c; }
+				break;
 
-char Menu_Admin() {
-	cout << "ADMIN MENU (Note: choices are case-insensitive.)\n\n"
-		<< "[-] Log out\n"
-		<< "[*] Change password\n"
-		<< "\n"
-		<< "STUDENTS AND CLASSES\n"
-		<< "[T1] Import students of a class (from a CSV file)\n"
-		<< "[T2] Add a student to a class\n"
-		<< "[T3] Change a student from class A to class B\n"
-		<< "[T4] Edit a student\n"
-		<< "[T5] Remove a student\n"
-		<< "[T6] View list of classes\n"
-		<< "[T7] View list of students in a class\n"
-		<< "\n"
-		<< "ACADEMIC YEARS AND SEMESTERS\n"
-		<< "[Y1] Create an academic year\n"
-		<< "[Y2] Update an academic year\n"
-		<< "[Y3] Delete an academic year\n"
-		<< "[Y4] View an academic year\n"
-		<< "[Y5] Create a semester\n"
-		<< "[Y6] Update a semester\n"
-		<< "[Y7] Delete a semester\n"
-		<< "[Y8] View a semester\n"
-		<< "\n"
-		<< "COURSES\n"
-		<< "[C1] Create a new course\n"
-		<< "[C2] Edit a course\n"
-		<< "[C3] Delete a course\n"
-		<< "[C4] Add a student to a course\n"
-		<< "[C5] Remove a student from a course\n"
-		<< "[C6] View list of courses in the current semester\n"
-		<< "[C7] View list of students of a course\n"
-		<< "\n"
-		<< "LECTURERS\n"
-		<< "[L1] Create a lecturer\n"
-		<< "[L2] Update a lecturer's info\n"
-		<< "[L3] Delete a lecturer\n"
-		<< "[L4] View all lecturers\n"
-		<< "\n"
-		<< "SCOREBOARDS\n"
-		<< "[S1] Search scoreboard of a course\n"
-		<< "[S2] View scoreboard of a course\n"
-		<< "[S3] Export a scoreboard (to a Csv file)\n"
-		<< "\n"
-		<< "ATTENDANCE LISTS\n"
-		<< "[A1] Search attendance list of a course\n"
-		<< "[A2] View attendance list of a course\n"
-		<< "[A3] Export attendance list (to a CSV file)\n"
-		<< endl;
+			case AC_ADMIN:
+				if (c == "t1") { cout << c; }
+				else if (c == "t2") { cout << c; }
+				else if (c == "t3") { cout << c; }
+				else if (c == "t4") { cout << c; }
+				else if (c == "t5") { cout << c; }
+				else if (c == "t6") { cout << c; }
+				else if (c == "t7") { cout << c; }
+				else if (c == "y1") { cout << c; }
+				else if (c == "y2") { cout << c; }
+				else if (c == "y3") { cout << c; }
+				else if (c == "y4") { cout << c; }
+				else if (c == "y5") { cout << c; }
+				else if (c == "y6") { cout << c; }
+				else if (c == "y7") { cout << c; }
+				else if (c == "y8") { cout << c; }
+				else if (c == "c1") { cout << c; }
+				else if (c == "c2") { cout << c; }
+				else if (c == "c3") { cout << c; }
+				else if (c == "c4") { cout << c; }
+				else if (c == "c5") { cout << c; }
+				else if (c == "c6") { cout << c; }
+				else if (c == "c7") { cout << c; }
+				else if (c == "l1") { cout << c; }
+				else if (c == "l2") { cout << c; }
+				else if (c == "l3") { cout << c; }
+				else if (c == "l4") { cout << c; }
+				else if (c == "s1") { cout << c; }
+				else if (c == "s2") { cout << c; }
+				else if (c == "s3") { cout << c; }
+				else if (c == "a1") { cout << c; }
+				else if (c == "a2") { cout << c; }
+				else if (c == "a3") { cout << c; }
+				break;
+		}
 
-	char r;
-	do {
-		cout << "> Choose: ";
-		string c;
-		cin >> c;
-		transform(c.begin(), c.end(), c.begin(), ::tolower);
+		cout << "\n";
 
-		if (c == "-") { r = -2; cout << "Logging out..."; }
-		else if (c == "*") { r = 0; cout << c; }
-		else if (c == "t1") { r = 0; cout << c; }
-		else if (c == "t2") { r = 0; cout << c; }
-		else if (c == "t3") { r = 0; cout << c; }
-		else if (c == "t4") { r = 0; cout << c; }
-		else if (c == "t5") { r = 0; cout << c; }
-		else if (c == "t6") { r = 0; cout << c; }
-		else if (c == "t7") { r = 0; cout << c; }
-		else if (c == "y1") { r = 0; cout << c; }
-		else if (c == "y2") { r = 0; cout << c; }
-		else if (c == "y3") { r = 0; cout << c; }
-		else if (c == "y4") { r = 0; cout << c; }
-		else if (c == "y5") { r = 0; cout << c; }
-		else if (c == "y6") { r = 0; cout << c; }
-		else if (c == "y7") { r = 0; cout << c; }
-		else if (c == "y8") { r = 0; cout << c; }
-		else if (c == "c1") { r = 0; cout << c; }
-		else if (c == "c2") { r = 0; cout << c; }
-		else if (c == "c3") { r = 0; cout << c; }
-		else if (c == "c4") { r = 0; cout << c; }
-		else if (c == "c5") { r = 0; cout << c; }
-		else if (c == "c6") { r = 0; cout << c; }
-		else if (c == "c7") { r = 0; cout << c; }
-		else if (c == "l1") { r = 0; cout << c; }
-		else if (c == "l2") { r = 0; cout << c; }
-		else if (c == "l3") { r = 0; cout << c; }
-		else if (c == "l4") { r = 0; cout << c; }
-		else if (c == "s1") { r = 0; cout << c; }
-		else if (c == "s2") { r = 0; cout << c; }
-		else if (c == "s3") { r = 0; cout << c; }
-		else if (c == "a1") { r = 0; cout << c; }
-		else if (c == "a2") { r = 0; cout << c; }
-		else if (c == "a3") { r = 0; cout << c; }
-		
-		else { r = -1; cout << "Invalid choice.\n"; }
-	} while (r == -1);
-	return r;
+	} while (1);
 }
--- Menu.h
+++ Menu.h
@@ -1,10 +1,10 @@
 #pragma once
-
 #include <iostream>
+#include <fstream>
 #include <string>
-#include <algorithm>
+#include <conio.h>
+#include "Login.h" // access classes and changing password
 using namespace std;
 
-char Menu_Student();
-char Menu_Lecturer();
-char Menu_Admin();
+void ShowMenu(const char & AccessClass); // to-do: include Student.h for info. Class?
+void Interpret(const string & ID, const char & AccessClass);
