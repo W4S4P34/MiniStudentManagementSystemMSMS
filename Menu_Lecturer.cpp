f246ee765c7f1d33c66ee63b3519bb9b74743aef
Thiện Bùi <thien.bui.84436@gmail.com>
2019-04-08 15:48:17 +0700

    wip: lecturer

--- Menu_Lecturer.cpp
+++ Menu_Lecturer.cpp
@@ -2,8 +2,11 @@
 
 void ShowHelp_Lecturer() {
 	cout << "\n[LECTURER COMMANDS]\n"
-		<< "clist    : View list of courses in a term\n"
-		<< "cslist   : View list of students of a course\n"
+		<< "ylist    : View list of academic years\n"
+		<< "tlist    : View list of terms in a year\s"
+		<< "rlist    : View list of courses in a term\n"
+		<< "rslist   : View list of students of a course\n"
+		<< "\n"
 		<< "alist    : View attendance list of a course\n"
 		<< "aedit    : Edit an attendance\n"
 		<< "scimport : Import scoreboard of a course\n"
@@ -23,7 +26,6 @@ void Menu_Lecturer(const string & ID) {
 	ShowHelp_Lecturer();
 	// End of start-up script
 
-	// Intepreter
 	do {
 		string c;
 		cout << "(" << ID << ")> ";
@@ -35,11 +37,12 @@ void Menu_Lecturer(const string & ID) {
 
 		if (c == "logout" || c == "-") { break; }
 		else if (c == "quit" || c == "exit") { exit(EXIT_SUCCESS); }
-		else if (c == "cls") { system("CLS"); }
+		else if (c == "cls") { system("CLS"); cout << "\n"; continue; }
 		else if (c == "info") { LookupLecturer(CurrentLecturerList, ID); }
-		else if (c == "help") { ShowHelp_General(); ShowHelp_Lecturer(); }
+		else if (c == "help") { ShowHelp_General(); ShowHelp_Admin(); }
 
-		else if (c == "passwd") {
+		else if (c == "passwd")
+		{
 			string Password_New, Password_New_Re;
 			cout << "Enter new password: ";
 			Password_New = GetPassword();
@@ -52,16 +55,59 @@ void Menu_Lecturer(const string & ID) {
 				cout << "Passwords do not match. Operation aborted.\n";
 			}
 			else {
-				ChangePassword(ID, AC_LECTURER, Password_New);
+				ChangePassword(ID, AC_ADMIN, Password_New);
 			}
 		}
 
-		//else if (c == "checkin") { }
-		//else if (c == "listck") { }
-		//else if (c == "sched") { }
-		//else if (c == "score") { }
+		else if (c == "ylist")
+		{
+			ListYears();
+		}
 
-		else { cout << "Invalid command.\n"; }
-		cout << "\n";
+		else if (c == "tlist")
+		{
+			string Year;
+			cout << "Enter year: "; getline(cin, Year); Capitalize(Year);
+			if (Year.empty() || Year == "\n") {
+				cout << "Empty values are invalid.";
+				continue;
+			}
+			ListTerms(Year);
+		}
+
+		else if (c == "rlist")
+		{
+			string Year, Term;
+			cout << "Enter year: "; getline(cin, Year); Capitalize(Year);
+			cout << "Enter term: "; getline(cin, Term); Capitalize(Term);
+			if (Year.empty() || Year == "\n" || Term.empty() || Term == "\n") {
+				cout << "Empty values are invalid.";
+				continue;
+			}
+			ListCourses(Year, Term);
+		}
+
+		// else if (c == "rslist")
+		
+		else if (c == "alist")
+		{
+			string Year, Term, CourseID, ClassID, DayOfWeek;
+			cout << "Enter year: "; getline(cin, Year); Capitalize(Year);
+			cout << "Enter term: "; getline(cin, Term); Capitalize(Term);
+			cout << "Enter course ID: "; getline(cin, CourseID); Capitalize(CourseID);
+			cout << "Enter class ID: "; getline(cin, ClassID); Capitalize(ClassID);
+			cout << "Enter day of week: "; getline(cin, DayOfWeek); LowerString(DayOfWeek);
+
+			if (
+				   Year.empty() || Year == "\n"
+				|| Term.empty() || Term == "\n"
+				|| CourseID.empty() || CourseID == "\n"
+				|| ClassID.empty() || ClassID == "\n"
+				|| 
+				)
+		}
+
+		else { cout << "Invalid command."; }
+		cout << "\n\n";
 	} while (1);
 }
